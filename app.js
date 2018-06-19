773a33237073b29a0ff91c93205c513618418816
Ricardo <ricardo.mertensibanez@gmail.com>
2018-06-18 21:07:39 -0300

    adds view engine

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var forceSsl = require('express-force-ssl');
 
+//
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+//
 var api = require('./routes/cdecbo');
 var app = express();
 
@@ -12,6 +20,19 @@ app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api', api);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/cdecbo.js
+++ routes/cdecbo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Express REST API');
+  res.render('index', { title: 'Express' });
 });
 
 router.get('/status', function(req, res, next) {
